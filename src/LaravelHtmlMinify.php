334e35fc26f475697a11bfb558941be2fa5ddc47
Mehmet Can <mehmetcan331997@gmail.com>
2023-04-30 20:46:00 +0300

    extra minify code

--- src/LaravelHtmlMinify.php
+++ src/LaravelHtmlMinify.php
@@ -43,8 +43,49 @@ class LaravelHtmlMinify
             # strip whitespaces between quotation ("') and end tags
             '/(\"|\')\s+\>/' =>  "$1>",
             # strip whitespaces between = "'
-            '/=\s+(\"|\')/' => "=$1"
+            '/=\s+(\"|\')/' => "=$1",
+            '%		# Collapse ws everywhere but in blacklisted elements.
+            (?>             # Match all whitespans other than single space.
+              [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
+            | \s{2,}        # or two or more consecutive-any-whitespace.
+            ) 				# Note: The remaining regex consumes no text at all...
+            (?=             # Ensure we are not in a blacklist tag.
+              (?:           # Begin (unnecessary) group.
+                (?:         # Zero or more of...
+                  [^<]++    # Either one or more non-"<"
+                | <         # or a < starting a non-blacklist tag.
+                  (?!/?(?:textarea|pre)\b)
+                )*+         # (This could be "unroll-the-loop"ified.)
+              )             # End (unnecessary) group.
+              (?:           # Begin alternation group.
+                <           # Either a blacklist start tag.
+                (?>textarea|pre)\b
+              | \z          # or end of file.
+              )             # End alternation group.
+            )  				# If we made it here, we are not in a blacklist tag.
+            %ix' => " "
         ];
-        return preg_replace(array_keys($replace), array_values($replace), $html);
+        $html = preg_replace(array_keys($replace), array_values($replace), $html);
+
+        
+        $html = preg_replace_callback('/(<[a-z\\-]+\\s)\\s*([^>]+>)/m', [$this, '_removeAttributeQuotes'], $html);
+
+        return $html;
+    }
+
+    function _removeAttributeQuotes($m) {	
+        $m[2] = preg_replace_callback('~([a-z0-9\\-])=(?<quote>[\'"])([^"\'\\s=]*)\k<quote>(\\s|>|/>)~i', [$this, '_removeAttributeQuotesCallback'], $m[2]);	
+        return $m[1].$m[2];
+    }
+            
+    function _removeAttributeQuotesCallback($m) {
+        if ($m[3] === '') {
+            return $m[1].$m[4];
+        }
+        if (/* 1 */ $m[4] == '/>' ||
+            /* 2 */ ($m[4] == '>' && substr($m[3], -1, 1) == '/')) {
+            return $m[1].'='.$m[3].' '.$m[4];
+        }
+        return $m[1].'='.$m[3].$m[4];
     }
 }
