ae7881f1218c05a439f1b3d4bad2c674ff286164
0x1881 <mehmetcan331997@gmail.com>
2023-06-21 02:51:39 +0300

    quotes slash fix, cdata remove, pre save add

--- src/LaravelHtmlMinify.php
+++ src/LaravelHtmlMinify.php
@@ -7,43 +7,71 @@ class LaravelHtmlMinify
   public function htmlMinify(string|null $html = null)
   {
     $replace = [
-      //remove tabs before and after HTML tags
-      '/\>[^\S ]+/s'                                                      => '>',
-      '/[^\S ]+\</s'                                                      => '<',
-      //shorten multiple whitespace sequences; keep new-line characters because they matter in JS!!!
-      '/([\t ])+/s'                                                       => ' ',
-      //remove leading and trailing spaces
-      '/^([\t ])+/m'                                                      => '',
-      '/([\t ])+$/m'                                                      => '',
+      // remove cdata in script tag
+      // https://stackoverflow.com/a/8283600/19275735
+      '~^\s*?//<!\[CDATA\[([\s\S]*)//\]\]>\s*?~m'                         => '$1',
+
       // remove JS line comments (simple only); do NOT remove lines containing URL (e.g. 'src="http://server.com/"')!!!
       '~//[a-zA-Z0-9 ]+$~m'                                               => '',
-      //remove empty lines (sequence of line-end and white-space characters)
+
+      // remove tabs before and after HTML tags
+      '/\>[^\S ]+/s'                                                      => '>',
+      '/[^\S ]+\</s'                                                      => '<',
+
+      // shorten multiple whitespace sequences; keep new-line characters because they matter in JS!!!
+      '/([\t ])+/s'                                                       => ' ',
+
+      // remove leading and trailing spaces
+      '/^([\t ])+/m'                                                      => '',
+      '/([\t ])+$/m'                                                      => '',
+
+
+      // remove empty lines (sequence of line-end and white-space characters)
       '/[\r\n]+([\t ]?[\r\n]+)+/s'                                        => "\n",
-      //remove empty lines (between HTML tags); cannot remove just any line-end characters because in inline JS they can matter!
+
+      // remove empty lines (between HTML tags); cannot remove just
+      // any line-end characters because in inline JS they can matter!
       '/\>[\r\n\t ]+\</s'                                                 => '><',
+
       //remove "empty" lines containing only JS's block end character; join with next line (e.g. "}\n}\n</script>" --> "}}</script>"
       '/}[\r\n\t ]+/s'                                                    => '}',
       '/}[\r\n\t ]+,[\r\n\t ]+/s'                                         => '},',
-      //remove new-line after JS's function or condition start; join with next line
+
+      // remove new-line after JS's function or condition start; join with next line
       '/\)[\r\n\t ]?{[\r\n\t ]+/s'                                        => '){',
       '/,[\r\n\t ]?{[\r\n\t ]+/s'                                         => ',{',
-      //remove new-line after JS's line end (only most obvious and safe cases)
-      '/\),[\r\n\t ]+/s'                                                  => '),',
-      //remove quotes from HTML attributes that does not contain spaces; keep quotes around URLs!
-      //'~([\r\n\t ])?([a-zA-Z0-9]+)=\"([a-zA-Z0-9_\\-]+)\"([\r\n\t ])?~s'  => '$1$2=$3$4',
 
-      '/(\n|^)(\x20+|\t)/' => "\n",
-      '/(\n|^)\/\/(.*?)(\n|$)/' => "\n",
-      '/\n/' => " ",
-      '/\<\!--.*?-->/' => "",
-      # Delete multispace (Without \n)
-      '/(\x20+|\t)/' => " ",
-      # strip whitespaces between tags
-      '/\>\s+\</' => "><",
-      # strip whitespaces between quotation ("') and end tags
-      '/(\"|\')\s+\>/' =>  "$1>",
-      # strip whitespaces between = "'
-      '/=\s+(\"|\')/' => "=$1",
+      // remove new-line after JS's line end (only most obvious and safe cases)
+      '/\),[\r\n\t ]+/s'                                                  => '),',
+
+      // remove places where quotes connect with a closing tag to avoid errors in the next step
+      '~\"/>~s'                                                           => '" />',
+
+      // remove quotes from HTML attributes that does not contain spaces; keep quotes around URLs!
+      // $1 and $4 insert first white-space character found before/after attribute
+      '~([\r\n\t ])?([a-zA-Z0-9]+)="([a-zA-Z0-9_/\\-]+)"([\r\n\t ])?~s'   => '$1$2=$3$4',
+
+      // remove any HTML comments, except MSIE conditional comments
+      '/\<\!--(?!\s*(?:\[if [^\]]+]|<!|>))(?:(?!-->).)*-->/s'             => '',
+
+      '/(\n|^)(\x20+|\t)/'                                                => "\n",
+      '/(\n|^)\/\/(.*?)(\n|$)/'                                           => "\n",
+
+      // replace end of line by a space
+      '/\n/'                                                              => " ",
+
+      // remove multispace (Without \n)
+      '/(\x20+|\t)/'                                                      => " ",
+
+      // strip whitespaces between tags
+      '/\>\s+\</'                                                         => "><",
+
+      // strip whitespaces between quotation ("') and end tags
+      '/(\"|\')\s+\>/'                                                    =>  "$1>",
+
+      // strip whitespaces between = "'
+      '/=\s+(\"|\')/'                                                     => "=$1",
+
       '%		# Collapse ws everywhere but in blacklisted elements.
             (?>             # Match all whitespans other than single space.
               [^\S ]\s*     # Either one [\t\r\n\f\v] and zero or more ws,
@@ -65,9 +93,15 @@ class LaravelHtmlMinify
             )  				# If we made it here, we are not in a blacklist tag.
             %ix' => " "
     ];
+
+    
+    [$pre, $html] = $this->savePre($html);
+    
     $html = preg_replace(array_keys($replace), array_values($replace), $html);
 
     $html = preg_replace_callback('/(<[a-z\\-]+\\s)\\s*([^>]+>)/m', [$this, '_removeAttributeQuotes'], $html);
+
+    $html = $this->restorePre($html, $pre);
 
     return $html;
   }
@@ -91,5 +125,27 @@ class LaravelHtmlMinify
       $matches[2]
     );
     return $matches[1] . $matches[2];
+  }
+
+  function savePre($input)
+  {
+    preg_match_all('|<pre(.*?)>(.*?)\</pre>|sim', $input, $pre);
+    foreach ($pre[0] as $key => $val) {
+      $input = str_replace($val, '<!~~SavePre' . $key . '~~>', $input);
+    }
+
+    return [
+      $pre,
+      $input
+    ];
+  }
+
+  function restorePre(&$output, $pre)
+  {
+    foreach ($pre[0] as $key => $val) {
+      $output = str_replace('<!~~SavePre' . $key . '~~>', $val, $output);
+    }
+
+    return $output;
   }
 }
